38bb97f86a00cd01d90bfc974e468395b9ad376d
Joan-Chamberlain <106273536+Joan-Chamberlain@users.noreply.github.com>
2023-06-14 23:37:15 -0600

    Cambios en el layout

--- DiarioWeb/Program.cs
+++ DiarioWeb/Program.cs
@@ -52,7 +52,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}" ,
+    pattern: "{controller=Home}/{action=DisplayPosts}/{id?}",
     app.MapRazorPages());
 
 using (var scope = app.Services.CreateScope())
